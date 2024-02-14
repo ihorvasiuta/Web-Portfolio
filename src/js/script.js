587d0136b2593cb2aa553564b2e7820e70fa2d40
ihorvasiuta <i.r.vasiuta@gmail.com>
2024-02-14 18:05:45 +0100

    changed structure of the project & updated version of navigation menu & implemented responsive footer

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle('open')
+  const isOpen = dropDownMenu.classList.contains('open')
+
+  // toggleBtnIcon.classList = isOpen ? ''
+}
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
